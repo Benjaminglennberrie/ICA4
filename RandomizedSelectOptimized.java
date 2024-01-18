75de0b6207addb72ab5674ceda4766d46c086dd7
Benjamin Brodwolf <benjaminglennberrie.brodwolf@calbaptist.edu>
2024-01-18 12:19:24 -0800

    working - Jan 17th

--- RandomizedSelectOptimized.java
+++ RandomizedSelectOptimized.java
@@ -1,64 +1,75 @@
 import java.util.Random;
 
+// This Java program demonstrates the Randomized Select algorithm, which efficiently finds the ith smallest element in an array.
+
 public class RandomizedSelectOptimized {
 
     public static void main(String[] args) {
-        int[] arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+        // Sample array
+        int[] inputArray = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
 
-        int i = 6; // Find the 6th smallest element
-        int result = randomizedSelect(arr, 0, arr.length - 1, i);
+        // Find the 6th smallest element
+        int targetPosition = 6;
+        int result = randomizedSelect(inputArray, 0, inputArray.length - 1, targetPosition);
 
-        System.out.println("The " + i + "th smallest element is: " + result);
+        // Print the result
+        System.out.println("The " + targetPosition + "th smallest element is: " + result);
     }
 
-    public static int randomizedSelect(int[] arr, int p, int r, int i) {
-        if (p == r) {
-            return arr[p];
+    // Randomized Select algorithm
+    public static int randomizedSelect(int[] arr, int startIndex, int endIndex, int targetPosition) {
+        // Base case: If there's only one element
+        if (startIndex == endIndex) {
+            return arr[startIndex];
         }
 
-        // Randomized Partition
-        int q = randomizedPartition(arr, p, r);
+        // Randomized Partition: Choose a random pivot and partition the array
+        int pivotIndex = randomizedPartition(arr, startIndex, endIndex);
 
         // Calculate the position of the pivot
-        int k = q - p + 1;
+        int pivotRelativePosition = pivotIndex - startIndex + 1;
 
-        if (i == k) {
-            return arr[q];
-        } else if (i < k) {
-            return randomizedSelect(arr, p, q - 1, i);
+        // Compare the position of the pivot with the desired position
+        if (targetPosition == pivotRelativePosition) {
+            return arr[pivotIndex]; // The pivot is the ith smallest element
+        } else if (targetPosition < pivotRelativePosition) {
+            return randomizedSelect(arr, startIndex, pivotIndex - 1, targetPosition); // Recursively search in the left subarray
         } else {
-            return randomizedSelect(arr, q + 1, r, i - k);
+            return randomizedSelect(arr, pivotIndex + 1, endIndex, targetPosition - pivotRelativePosition); // Recursively search in the right subarray
         }
     }
 
-    public static int randomizedPartition(int[] arr, int p, int r) {
-        // Randomly choose a pivot index and swap with the last element
-        int randomIndex = new Random().nextInt(r - p + 1) + p;
-        swap(arr, randomIndex, r);
+    // Randomized Partition: Randomly choose a pivot index and swap with the last element
+    public static int randomizedPartition(int[] arr, int startIndex, int endIndex) {
+        int randomIndex = new Random().nextInt(endIndex - startIndex + 1) + startIndex; // Random pivot index
+        swap(arr, randomIndex, endIndex); // Swap the pivot with the last element
 
-        return partition(arr, p, r);
+        return partition(arr, startIndex, endIndex); // Call the regular partition function
     }
 
-    public static int partition(int[] arr, int p, int r) {
-        int pivot = arr[r];
-        int i = p - 1;
+    // Partition the array around a pivot
+    public static int partition(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[endIndex]; // Choose the last element as the pivot
+        int i = startIndex - 1; // Index of smaller element
 
-        for (int j = p; j < r; j++) {
+        // Traverse the array and rearrange elements around the pivot
+        for (int j = startIndex; j < endIndex; j++) {
             if (arr[j] <= pivot) {
                 i++;
                 swap(arr, i, j);
             }
         }
 
-        // Swap pivot to its correct position
-        swap(arr, i + 1, r);
+        // Swap the pivot to its correct position
+        swap(arr, i + 1, endIndex);
 
-        return i + 1;
+        return i + 1; // Return the index of the pivot
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    // Swap two elements in the array
+    public static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 }
